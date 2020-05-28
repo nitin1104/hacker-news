c8b424ae6211093f0bdf1d8bb4433cf193521fc6
Nitin Bhatnagar <nitin.web@gmail.com>
2020-05-29 02:01:36 +0530

    WIP error fix

--- src/components/news/News.js
+++ src/components/news/News.js
@@ -3,32 +3,32 @@ import Moment from 'moment';
 import './News.scss';
 
 function News (props) { 
-
     function getShortURL(url) {
         return url.replace(/https?:\/\/(www\.)?/, '').split('/')[0];
     }
-
     return (
         <Fragment>
             <ul className="newsContainer">
                 {props.newsData.map(item => {
                     if (item.title) {
-                        return (
-                            <li key={item.objectID} className="news-block">
-                                <div className="comments">{item.num_comments}</div>
-                                <div>{item.points}</div>
-                                <div className="votes">
-                                    <span className="vote">{item.points}</span>
-                                    <button className="vote-icon">upvote</button>
-                                </div>
-                                <div className="title">{item.title}</div>
-                                <div className="additional-info">{item.url ? '(' + getShortURL(item.url) + ')' : ''}</div>
-                                <div className="additional-info">Create : {Moment(item.created_at).fromNow()}</div>
-                                <div className="hide-info">
-                                    <button className="hide-link">[ Hide ]</button>
-                                </div>
-                            </li>
-                        )
+                      return (
+                        <li key={item.objectID} className="news-block">
+                            <div className="comments">{item.num_comments}</div>
+                            <div>{item.points}</div>
+                            <div className="votes">
+                                <span className="vote">{item.points}</span>
+                                <button className="vote-icon">upvote</button>
+                            </div>
+                            <div className="title">{item.title}</div>
+                            <div className="additional-info">{item.url ? '(' + getShortURL(item.url) + ')' : ''}</div>
+                            <div className="additional-info">Create : {Moment(item.created_at).fromNow()}</div>
+                            <div className="hide-info">
+                                <button className="hide-link">[ Hide ]</button>
+                            </div>
+                        </li>
+                        )  
+                    } else {
+                        return null;
                     }
                 })} 
             </ul>
