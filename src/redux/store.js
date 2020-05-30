17e60dd24803e72a01fb3e7fa5548a61419cca92
Nitin Bhatnagar <nitin.web@gmail.com>
2020-05-31 01:53:20 +0530

    TODO - Pagination, Hide, Upvote

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,13 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 
 import rootReducer from './reducer';
-
+ /* eslint-disable */
 const initialState = {
     news: {
         hits: []
     }
 }
+
 export const configureStore = (initialState = initialState) => createStore(
     rootReducer,
     initialState,
